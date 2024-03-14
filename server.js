d7fa324ade697ca08211fcf07b33329309276622
Mohamad <mohmaddalil5@gmail.com>
2024-03-14 18:54:56 +0300

    connecting to online database

--- server.js
+++ server.js
@@ -10,9 +10,18 @@ process.on('uncaughtException', err => {
 dotenv.config({path: './config.env'});
 const app = require('./app');
 
-mongoose.connect(process.env.DATABASE_LOCAL).then(() => {
-    console.log('connected to database successfuly!');
-});
+if (process.env.NODE_ENV === 'development') {
+    mongoose.connect(process.env.DATABASE_LOCAL).then(() => {
+        console.log('connected to database successfuly!');
+    });
+}
+else {
+    const DB = process.env.MONGODB_URI.replace('<password>', process.env.MONGODB_KEY);
+    mongoose.connect(DB)
+    .then(() => {
+        console.log('connected to database successfuly!');
+    });
+}
 
 app.listen(process.env.PORT, () => {
     console.log(`app is running on port ${process.env.PORT}...`);
