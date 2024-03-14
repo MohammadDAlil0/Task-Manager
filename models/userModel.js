1ba4ff457aaed51c39ea454ecbf7821610f76667
Mohamad <mohmaddalil5@gmail.com>
2024-03-14 13:15:28 +0300

    removing an extra attriubte

--- models/userModel.js
+++ models/userModel.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
 const userSchema = mongoose.Schema({
-    name: String,
     username: {
         type: String,
         unique: true,
